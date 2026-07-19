4ee909945ed0201feee4dc3941c296a25accd7af
AbdulQader Qassab <abdulqader.qassab@constructor.io>
2026-07-19 19:19:58 +0400

    refactor(worker): package manatal sync workflow

--- worker/src/cv_intelligence_worker/workflows/__init__.py
+++ worker/src/cv_intelligence_worker/workflows/__init__.py
@@ -2,12 +2,15 @@
 
 from .draft_ingestion import DraftIngestion
 from .manatal_originals import ManatalOriginalsBackfill, ManatalOriginalsBackfillResult
+from .manatal_sync import ManatalSync, ManatalSyncResult
 from .public_applications import PublicApplicationIngestion, PublicApplicationIngestionResult
 
 __all__ = [
     "DraftIngestion",
     "ManatalOriginalsBackfill",
     "ManatalOriginalsBackfillResult",
+    "ManatalSync",
+    "ManatalSyncResult",
     "PublicApplicationIngestion",
     "PublicApplicationIngestionResult",
 ]
